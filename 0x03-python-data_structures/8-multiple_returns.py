304a0c3b2264b5196cb4ed27f1d4ce278f3cbe3c
Sameezub <sameezub@gmail.com>
2022-12-03 23:03:27 +0000

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0]
+    length = len(sentence)
+    if length == 0:
+        result = (0, None)
+        return result
+    else:
+        res = (length, sentence[0:1])
+        return res
